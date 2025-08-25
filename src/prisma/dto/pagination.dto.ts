9d2d8bde0b27fc913433ad24368c2f8803a8383e
Dandy Candra <dandycandrapratama@gmail.com>
2025-08-25 10:33:29 +0700

    feat: add QueryPagination DTO for handling pagination parameters

---
+++ src/prisma/dto/pagination.dto.ts
@@ -0,0 +1,13 @@
+import { IsOptional } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { toNumeric } from 'utils/toNumeric';
+
+export class QueryPagination {
+  @IsOptional()
+  @Transform(toNumeric)
+  page: number = 1;
+
+  @IsOptional()
+  @Transform(toNumeric)
+  limit: number = 10;
+}
