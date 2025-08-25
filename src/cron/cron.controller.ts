05951f8fb61fed32fb750bda99bec1de28763a13
Dandy Candra <dandycandrapratama@gmail.com>
2025-08-25 10:33:37 +0700

    feat: add CronModule and CronController for handling scheduled tasks

---
+++ src/cron/cron.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Param, UnauthorizedException } from '@nestjs/common';
+import { Public } from 'src/auth/decorator/public.decorator';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Controller('cron')
+export class CronController {
+  constructor(private prisma: PrismaService) {}
+
+  @Public()
+  @Get('check-expired-task/:secret')
+  async handleExpireCheck(@Param('secret') secret: string) {
+    if (secret !== process.env.CRON_SECRET) {
+      throw new UnauthorizedException('Invalid secret');
+    }
+
+    const updatedAssignment = await this.prisma.classAssignments.updateMany({
+      where: {
+        due_date: { lt: new Date() },
+        is_available: true,
+      },
+      data: { is_available: false },
+    });
+
+    const updatedQuiz = await this.prisma.classQuiz.updateMany({
+      where: {
+        due_date: { lt: new Date() },
+        is_available: true,
+      },
+      data: { is_available: false },
+    });
+
+    return {
+      message: 'Expired check executed',
+      data: { updatedAssignment, updatedQuiz },
+    };
+  }
+}
