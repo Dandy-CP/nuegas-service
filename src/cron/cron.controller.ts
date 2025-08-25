564d78a528678c846a5e9e0f67d3d529d8a9707f
Dandy Candra <dandycandrapratama@gmail.com>
2025-08-25 13:38:11 +0700

    fix: remove secret validation from handleExpireCheck method

--- src/cron/cron.controller.ts
+++ src/cron/cron.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { Public } from '../auth/decorator/public.decorator';
 import { PrismaService } from '../prisma/prisma.service';
 
@@ -8,11 +8,7 @@ export class CronController {
 
   @Public()
   @Get('check-expired-task')
-  async handleExpireCheck(@Query('secret') secret: string) {
-    if (secret !== process.env.CRON_SECRET) {
-      throw new UnauthorizedException('Invalid secret');
-    }
-
+  async handleExpireCheck() {
     const updatedAssignment = await this.prisma.classAssignments.updateMany({
       where: {
         due_date: { lt: new Date() },
